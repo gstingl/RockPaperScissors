a84721a910633fe5c2c9c0053e7768333b0bb5aa
Garret Stingl <garretstingl21@gmail.com>
2024-06-22 11:48:25 -0500

    Create function getComputerChoice

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// Get Computer Choice
+// Get Computer Choice by getting random number between 0-2
+function getComputerChoice() {
+  if (Math.floor(Math.random() * 3) == 0) {
+    return "Rock";
+  } else if (Math.floor(Math.random() * 3) == 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
 
+console.log(getComputerChoice());
